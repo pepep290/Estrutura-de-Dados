f57516ffc0396d3ac226e31ded26499f76f41b0e
Pedro Vitor Cruz <vitorpedro2904@gmail.com>
2026-03-10 14:25:56 -0300

    Update func.h

--- Tp1/func.h
+++ Tp1/func.h
@@ -1,9 +1,6 @@
 #ifndef func_h
 #define func_h
 
-//Pedro Vítor Silva Cruz 24.2.4086
-//Rafael Araújo Granato 24.2.4085
-
 typedef struct tabuleiro Tab;
 //  1.LeituraTabuleiro: inicializa tabuleiro a partir de dados do terminal.
 //  2. TabuleiroEhValido: retorna se o tabuleiro é válido ou não.
@@ -17,5 +14,6 @@ char Venceu(Tab* tabuleiro);
 int Empate(Tab* tabuleiro);
 int JogadaMestre(Tab* tabuleiro, int coordernadas[2][2]);
 char proximoJogador(Tab* tabuleiro);
+
 
 #endif // func_h
